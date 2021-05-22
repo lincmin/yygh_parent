866df331a17b0ddb9e02e309c0ee2f6f57225b1a
lincmin <lincm69696@qq.com>
2021-05-22 14:06:16 +0800

    添加HospitalSetService接口及HospitalSetServiceImpl实现类

---
+++ service/service_hosp/src/main/java/com/lincm/yygh/hosp/service/HospitalSetService.java
@@ -0,0 +1,7 @@
+package com.lincm.yygh.hosp.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lincm.yygh.model.hosp.HospitalSet;
+
+public interface HospitalSetService extends IService<HospitalSet> {
+}
---
+++ service/service_hosp/src/main/java/com/lincm/yygh/hosp/service/impl/HospitalSetServiceImpl.java
@@ -0,0 +1,11 @@
+package com.lincm.yygh.hosp.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lincm.yygh.hosp.mapper.HospitalSetMapper;
+import com.lincm.yygh.hosp.service.HospitalSetService;
+import com.lincm.yygh.model.hosp.HospitalSet;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HospitalSetServiceImpl extends ServiceImpl<HospitalSetMapper, HospitalSet> implements HospitalSetService {
+}
