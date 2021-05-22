efdcb785c221c1cfd45e8d74fe45a93e9f05db58
lincmin <lincm69696@qq.com>
2021-05-22 17:56:33 +0800

    配置@MapperScan

---
+++ service/service_hosp/src/main/java/com/lincm/yygh/hosp/config/HospConfig.java
@@ -0,0 +1,9 @@
+package com.lincm.yygh.hosp.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.lincm.yygh.hosp.mapper")
+public class HospConfig {
+}
