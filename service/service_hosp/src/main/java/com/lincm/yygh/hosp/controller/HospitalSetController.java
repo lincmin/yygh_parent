a7d11494d37bc05b2ccbe5416aef246507e5de5c
lincmin <lincm69696@qq.com>
2021-05-22 19:01:24 +0800

    创建控制器,查询医院设置表所有信息

--- service/service_hosp/src/main/java/com/lincm/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/lincm/yygh/hosp/controller/HospitalSetController.java
@@ -1,9 +1,13 @@
 package com.lincm.yygh.hosp.controller;
 
 import com.lincm.yygh.hosp.service.HospitalSetService;
+import com.lincm.yygh.model.hosp.HospitalSet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
@@ -11,4 +15,15 @@ public class HospitalSetController {
     //注入service
     @Autowired
     private HospitalSetService hospitalSetService;
+
+    //访问路径http://localhost:8201/admin/hosp/hospitalSet/findAll
+
+    //查询医院设置表所有信息
+    @GetMapping("findAll")
+    public List<HospitalSet> findAllHospitalSet(){
+        //调用service方法
+        List<HospitalSet> list = hospitalSetService.list();
+        //list转换json返回
+        return list;
+    }
 }
